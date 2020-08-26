a3c43515984cc0dca65b75035e98b6dfa7139326
bedlamzd <bedlamzd@gmail.com>
2020-08-26 20:58:34 +0300

    style changes; replaced echo with printf to use \n; added quotes to support spaces and other symbols in arguments;

--- script.sh
+++ script.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+
 printMenu(){
 	
 cat << END
 1. Напечатать имя текущего каталога
 2. Сменить текущий каталог
-3. Выдать список пользователей имеющих хотя бы один процесс
+3. Выдать список пользователей, имеющих хотя бы один процесс
 4. Создать файл
 5. Скопировать файл
 6. Выйти из программы
+
 END
 
 	return
@@ -17,31 +20,35 @@ do
 	printMenu
 	read -p "Введите номер пункта из меню: " opt
 	case $opt in
-	1)
-		echo `pwd`
-		;;
-	2)
-		read -p "Куда идём? " newdir
-		cd $newdir
-		;;
-	3)
-		# TODO
-		echo "Пока не умею"
-		;;
-	4)
-		read -p "Введите имя файла: " filename
-		touch $filename
-		;;
-	5)
-		read -p "Что копируем? " filename
-		read -p "Куда копируем? " path
-		cp $filename $path
-		;;
-	6)
-		exit 0
-		;;
-	*)
-		echo "Нет такого пункта в меню."
-		;;
+		1)
+			printf "`pwd`\n\n"
+			;;
+		2)
+			read -p "Путь до новой директории: " newdir
+			cd "$newdir"
+			printf "Текущий каталог сменён на `pwd`\n\n"
+			;;
+		3)
+			# TODO
+			printf 'Пока не умею\n\n'
+			;;
+		4)
+			read -p "Введите имя файла: " filename
+			touch "$filename"
+			printf "Файл $filename создан\n\n"
+			;;
+		5)
+			read -p "Файл для копирования: " filename
+			read -p "Путь копирования: " path
+			cp "$filename" "$path"
+			printf "Файл $filename скопирован в $path\n\n"
+			;;
+		6)
+			echo "Осуществляю выход из программы."
+			exit 0
+			;;
+		*)
+			printf "Нет такого пункта в меню.\n\n"
+			;;
 	esac
 done
